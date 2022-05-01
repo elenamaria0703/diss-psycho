6dd116b46c5d4b4334beff679e01e7acd0206348
ioanarotaru29 <ioanarotaru29@yahoo.com>
2022-05-01 17:54:07 +0300

    Login Page Basics

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,23 @@ import './App.css';
 import {PrivateRoute} from "./shared/auth/PrivateRoute";
 import Login from "./shared/auth/Login";
 import StudentMainPage from "./students/StudentMainPage";
+import 'bootstrap/dist/css/bootstrap.css';
+import {AuthProvider} from "./shared/auth/AuthProvider";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route
-                path="student"
-                element={<PrivateRoute roles={['student']} component={StudentMainPage} />}
-            />
-          </Routes>
-      </Router>
+     <AuthProvider>
+         <Router>
+              <Routes>
+                <Route path="/" element={<Login />} />
+                <Route
+                    path="student"
+                    element={<PrivateRoute roles={['student']} component={StudentMainPage} />}
+                />
+              </Routes>
+          </Router>
+     </AuthProvider>
     </div>
   );
 }
